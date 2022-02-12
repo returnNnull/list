3aa523e99e9affad4a841f027607640f3f8f5444
user <user@WIN-BDAHPTRT5BE>
2022-02-12 21:27:08 +0300

    Добавьте файлы проекта.

---
+++ list/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace List
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {6, 12, -3, 44, 76, 5, 100 };
+            
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i] % 2 != 0)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+
+            PrintCollection(list);
+            Console.ReadKey();
+        }
+
+        private static void PrintCollection(List<int> list)
+        {
+            foreach ( int item in list )
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+}
+
+//15.Из созданного списка удалить каждый нечетный элемент.
